330ff92cc328b4c55ffd46126eccdf86fe3c6e3b
Fredrik Johansen <fredrik1206@gmail.com>
2024-05-18 22:06:36 +0200

    feat(studio): changes ordering of sidebar and adds project document

--- deskStructure.ts
+++ deskStructure.ts
@@ -1,4 +1,4 @@
-import {CaseIcon, CommentIcon, DocumentsIcon, EditIcon, UserIcon} from '@sanity/icons'
+import {CaseIcon, CommentIcon, DocumentsIcon, EditIcon, UserIcon, PackageIcon} from '@sanity/icons'
 import {StructureBuilder} from 'sanity/desk'
 
 export const myStructure = (S: StructureBuilder) =>
@@ -14,7 +14,9 @@ export const myStructure = (S: StructureBuilder) =>
         .icon(CaseIcon)
         .child(S.document().schemaType('resume').documentId('resume')),
       S.divider(),
+      S.listItem().title('Testimonials').icon(CommentIcon).child(S.documentTypeList('testimonial')),
+      S.listItem().title('Projects').icon(PackageIcon).child(S.documentTypeList('project')),
+      S.divider(),
       S.listItem().title('Blog Series').icon(DocumentsIcon).child(S.documentTypeList('blogSeries')),
       S.listItem().title('Blog Posts').icon(EditIcon).child(S.documentTypeList('blogPost')),
-      S.listItem().title('Testimonials').icon(CommentIcon).child(S.documentTypeList('testimonial')),
     ])
